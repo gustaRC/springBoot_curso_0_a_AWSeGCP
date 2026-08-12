204e999dcccd39f8d18460eda48a0f539f5d2ea4
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2026-08-12 14:52:57 -0300

    Adicionado detalhes na documentação do swagger nas requisições de findById e findAll

--- src/main/java/br/com/beltsistemas/springBoot_curso_0_a_AWSeGCP/controllers/PersonController.java
+++ src/main/java/br/com/beltsistemas/springBoot_curso_0_a_AWSeGCP/controllers/PersonController.java
@@ -3,6 +3,12 @@ package br.com.beltsistemas.springBoot_curso_0_a_AWSeGCP.controllers;
 import br.com.beltsistemas.springBoot_curso_0_a_AWSeGCP.data.dto.v1.PersonDTO;
 import br.com.beltsistemas.springBoot_curso_0_a_AWSeGCP.data.dto.v2.PersonDTOV2;
 import br.com.beltsistemas.springBoot_curso_0_a_AWSeGCP.services.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +19,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/person/v1")
+@Tag(name = "Person Endpoint", description = "Endpoints for Managing Person API")
 public class PersonController {
 
     @Autowired
     private PersonService service; // == private PersonService service = new PersonService();
 
     @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE } /* produces -> dados ENVIADOS DA a API (response) */ )
+    @Operation(
+            summary = "Finds all people",
+            description = "Finds all people",
+            tags = {"Person"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))) }),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            }
+    )
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
@@ -26,6 +46,19 @@ public class PersonController {
     @GetMapping(
             value = "/{id}",
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE } /* produces -> dados ENVIADOS DA a API (response) */
+    )
+    @Operation(
+            summary = "Finds a person by ID",
+            description = "Finds a person by ID",
+            tags = {"Person"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PersonDTO.class)) }),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            }
     )
     public PersonDTO findById(
             @PathVariable String id //@PathVariable indica a necessidade de preenchimento do caminho da URL: '/person/{id}'
