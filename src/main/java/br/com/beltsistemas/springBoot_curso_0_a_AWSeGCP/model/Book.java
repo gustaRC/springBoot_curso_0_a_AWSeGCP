3376a089204d6ee2ed57283f4c48f90949f6d009
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2026-08-13 14:01:33 -0300

    Criado entidade Book

---
+++ src/main/java/br/com/beltsistemas/springBoot_curso_0_a_AWSeGCP/model/Book.java
@@ -0,0 +1,89 @@
+package br.com.beltsistemas.springBoot_curso_0_a_AWSeGCP.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private Date launch_date;
+
+    @Column(nullable = false, precision = 65, scale = 2)
+    private BigDecimal price;
+
+    private String title;
+
+    public Book() {
+    }
+
+    public Book(String author, int id, Date launch_date, BigDecimal price, String title) {
+        setAuthor(author);
+        setId(id);
+        setLaunch_date(launch_date);
+        setPrice(price);
+        setTitle(title);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getLaunch_date() {
+        return launch_date;
+    }
+
+    public void setLaunch_date(Date launch_date) {
+        this.launch_date = launch_date;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Book book)) return false;
+        return getId() == book.getId() && Objects.equals(getAuthor(), book.getAuthor()) && Objects.equals(getLaunch_date(), book.getLaunch_date()) && Objects.equals(getPrice(), book.getPrice()) && Objects.equals(getTitle(), book.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getAuthor(), getLaunch_date(), getPrice(), getTitle());
+    }
+}
